47b345d5c36ba1455092ec2622d2bd830e845d02
saeed ranjbar <seed.rb71@gmail.com>
2018-11-18 08:57:28 +0330

    added some change on masterpage

--- resources/views/admin/master/masterpage.blade.php
+++ resources/views/admin/master/masterpage.blade.php
@@ -260,31 +260,6 @@
                         <!-- Page-Title -->
                         <div class="row">
                             <div class="col-sm-12">
-                                <div class="btn-group pull-right m-t-15">
-                                    <button type="button" class="btn btn-default dropdown-toggle waves-effect waves-light" data-toggle="dropdown" aria-expanded="false">Settings <span class="m-l-5"><i class="fa fa-cog"></i></span></button>
-                                    <ul class="dropdown-menu drop-menu-right" role="menu">
-                                        <li><a href="#">Action</a></li>
-                                        <li><a href="#">Another action</a></li>
-                                        <li><a href="#">Something else here</a></li>
-                                        <li class="divider"></li>
-                                        <li><a href="#">Separated link</a></li>
-                                    </ul>
-                                </div>
-
-                                <h4 class="page-title">Small menu</h4>
-                                <ol class="breadcrumb">
-									<li>
-										<a href="#">Ubold</a>
-									</li>
-									<li>
-										<a href="#">Layouts</a>
-									</li>
-									<li class="active">
-										Small menu
-									</li>
-								</ol>
-                            </div>
-                            <div class="col-sm-12">
                                 @yield('content')
                             </div>
                         </div>
