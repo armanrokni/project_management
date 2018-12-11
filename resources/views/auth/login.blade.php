93cbf4cfd28e175b66403658c71fdd5b008abfaa
Elham Sadeghian <elhamsadeghian72@gmail.com>
2018-12-11 13:04:25 +0330

    adding auth files

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
+
+        <link rel="shortcut icon" href="{{asset('')}}public/adminPanel/assets/images/favicon_1.ico">
+
+        <title></title>
+
+        <link href="{{asset('')}}public/adminPanel/assets/css/bootstrap-rtl.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('')}}public/adminPanel/assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('')}}public/adminPanel/assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('')}}public/adminPanel/assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('')}}public/adminPanel/assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('')}}public/adminPanel/assets/css/responsive.css" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+
+        <script src="{{asset('')}}public/adminPanel/assets/js/modernizr.min.js"></script>
+
+    </head>
+    <body>
+
+        <div class="account-pages"></div>
+        <div class="clearfix"></div>
+        <div class="wrapper-page">
+        	<div class=" card-box">
+            <div class="panel-heading">
+                <h3 class="text-center"> ورود به  <strong class="text-custom">آوند</strong> </h3>
+            </div>
+
+
+            <div class="panel-body">
+            <form class="form-horizontal m-t-20" action="{{ route('login') }}" method="post">
+                @csrf
+
+                <div class="form-group ">
+                    <div class="col-xs-12">
+                        <input class="form-control" type="text" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" required="" name="phone" placeholder="{{ __('شماره همراه') }}" value="{{ old('phone') }}" required autofocus>
+                            @if ($errors->has('phone'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('phone') }}</strong>
+                                </span>
+                            @endif
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-xs-12">
+                        <input class="form-control" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password" required="" name="password" placeholder="{{ __('کلمه عبور') }}">
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                    </div>
+                </div>
+
+                <div class="form-group ">
+                    <div class="col-xs-12">
+                        <div class="checkbox checkbox-primary">
+                            <input id="checkbox-signup" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <label for="checkbox-signup">
+                                {{ __('مرا بخاطر بسپار') }}
+                            </label>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="form-group text-center m-t-40">
+                    <div class="col-xs-12">
+                        <button class="btn btn-green-dark btn-block text-uppercase waves-effect waves-light" type="submit">{{ __('ورود') }}</button>
+                    </div>
+                </div>
+
+                <div class="form-group m-t-30 m-b-0">
+                    <div class="col-sm-12">
+                        <a href="{{ route('password.update') }}" class="text-dark"><i class="fa fa-lock m-r-5"></i> {{ __('رمز عبور خود را فراموش کرده‌اید؟') }}</a>
+                    </div>
+                </div>
+            </form>
+
+            </div>
+            </div>
+        </div>
+
+
+
+
+    	<script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="{{asset('')}}public/adminPanel/assets/js/jquery.min.js"></script>
+        <script src="{{asset('')}}public/adminPanel/assets/js/bootstrap-rtl.min.js"></script>
+        <script src="{{asset('')}}public/adminPanel/assets/js/detect.js"></script>
+        <script src="{{asset('')}}public/adminPanel/assets/js/fastclick.js"></script>
+        <script src="{{asset('')}}public/adminPanel/assets/js/jquery.slimscroll.js"></script>
+        <script src="{{asset('')}}public/adminPanel/assets/js/jquery.blockUI.js"></script>
+        <script src="{{asset('')}}public/adminPanel/assets/js/waves.js"></script>
+        <script src="{{asset('')}}public/adminPanel/assets/js/wow.min.js"></script>
+        <script src="{{asset('')}}public/adminPanel/assets/js/jquery.nicescroll.js"></script>
+        <script src="{{asset('')}}public/adminPanel/assets/js/jquery.scrollTo.min.js"></script>
+
+
+        <script src="{{asset('')}}public/adminPanel/assets/js/jquery.core.js"></script>
+        <script src="{{asset('')}}public/adminPanel/assets/js/jquery.app.js"></script>
+
+	</body>
+</html>
---
+++ resources/views/auth/passwords/reset.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
+
+        <link rel="shortcut icon" href="{{asset('')}}public/adminPanel/assets/images/favicon_1.ico">
+
+        <title></title>
+
+        <link href="{{asset('')}}public/adminPanel/assets/css/bootstrap-rtl.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('')}}public/adminPanel/assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('')}}public/adminPanel/assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('')}}public/adminPanel/assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('')}}public/adminPanel/assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('')}}public/adminPanel/assets/css/responsive.css" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+
+        <script src="{{asset('')}}public/adminPanel/assets/js/modernizr.min.js"></script>
+
+    </head>
+    <body>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('بازیابی رمزعبور') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('password.reset') }}">
+                        @csrf
+
+                        <input type="hidden" name="token" value="{{ $token }}">
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('پست الکترونیک') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('رمزعبور') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('تایید رمزعبور') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('بازیابی رمزعبور') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+      var resizefunc = [];
+  </script>
+
+  <!-- jQuery  -->
+  <script src="{{asset('')}}public/adminPanel/assets/js/jquery.min.js"></script>
+  <script src="{{asset('')}}public/adminPanel/assets/js/bootstrap-rtl.min.js"></script>
+  <script src="{{asset('')}}public/adminPanel/assets/js/detect.js"></script>
+  <script src="{{asset('')}}public/adminPanel/assets/js/fastclick.js"></script>
+  <script src="{{asset('')}}public/adminPanel/assets/js/jquery.slimscroll.js"></script>
+  <script src="{{asset('')}}public/adminPanel/assets/js/jquery.blockUI.js"></script>
+  <script src="{{asset('')}}public/adminPanel/assets/js/waves.js"></script>
+  <script src="{{asset('')}}public/adminPanel/assets/js/wow.min.js"></script>
+  <script src="{{asset('')}}public/adminPanel/assets/js/jquery.nicescroll.js"></script>
+  <script src="{{asset('')}}public/adminPanel/assets/js/jquery.scrollTo.min.js"></script>
+
+
+  <script src="{{asset('')}}public/adminPanel/assets/js/jquery.core.js"></script>
+  <script src="{{asset('')}}public/adminPanel/assets/js/jquery.app.js"></script>
+
+</body>
+</html>
