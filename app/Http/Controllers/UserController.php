0163b0f3eaef95f78b41ab4ffbf1027ceb7121d2
Elham Sadeghian <elhamsadeghian72@gmail.com>
2018-12-11 12:46:34 +0330

    changing UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use App\User;
 use App\Expertise;
+use App\lib\Jdf;
 
 class UserController extends Controller
 {
@@ -13,12 +15,17 @@ class UserController extends Controller
     return View('admin/userManagement/all' , ['users' => User::with('Expertise')->get()]);
   }
 
-  public function insertView(){
-    $expertise = Expertise::get();
-    return View('admin.userManagement.insert',['expertises' => $expertise]);
-  }
+
 
   public function insert(Request $request){
+    \Validator::make($request->all(), [
+      'fullname' => 'required|max:255',
+      'phone' => 'required|numeric',
+      'email' => 'required|e-mail',
+      'avatar' => 'required|image',
+      'lastLogin' => 'nullable',
+      'access[]' => 'nullable'
+    ])->validate();
 
     $avatar = $request->file('avatar');
     $extension = $avatar->getClientOriginalExtension();
@@ -29,21 +36,40 @@ class UserController extends Controller
 
     $user = new User($request->all());
     $user->avatar = 'uploads/logos/'.$avatarname;
-    $user->password = 1234;
+    $user->password = \Hash::make('1234');
     $user->save();
     return redirect('admin/users');
+  }
+
+  public function insertView(){
+    $expertise = Expertise::get();
+    return View('admin.userManagement.insert',['expertises' => $expertise]);
   }
 
   public function edit($id)
   {
     $user = User::find($id);
-    return view('admin.userManagement.edit', ['user'=> $user, 'expertises' => Expertise::get()]);
+    $access = explode(',' , $user->access);
+    return view('admin.userManagement.edit', ['user'=> $user, 'expertises' => Expertise::get() , 'access' => $access]);
   }
 
   public function update(Request $request)
   {
+    \Validator::make($request->all(), [
+      'fullname' => 'required|max:255',
+      'phone' => 'required|numeric',
+      'email' => 'required|e-mail',
+      'avatar' => 'image',
+      'lastLogin' => 'nullable',
+      'access[]' => 'nullable'
+    ])->validate();
 
     $user = User::find($request->id);
+    $user->fullname=$request->get('fullname');
+    $user->phone=$request->get('phone');
+    $user->email=$request->get('email');
+    $user->lastLogin=$request->get('lastLogin');
+
     if($request->hasfile('avatar')){
     $image = public_path($user->avatar);
     if (\File::exists($image)) {
@@ -59,7 +85,8 @@ class UserController extends Controller
     //$request->merge(['access' => implode(',', (array) $request->get('access'))]);
 
     }
-  //echo  implode(',' , $request->access);die;
+    //echo  implode(',' , $request->access);die;
+    // print_r($request->access);die;
     $user->access = implode(',' , $request->access);
     $user->save();
     return redirect('admin/users');
