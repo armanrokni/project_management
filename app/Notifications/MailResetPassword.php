7e16a31959876280c97ecdb9a7cf705016acec67
Elham Sadeghian <elhamsadeghian72@gmail.com>
2018-12-11 13:02:57 +0330

    adding Notification

---
+++ app/Notifications/MailResetPassword.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class MailResetPassword extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+      $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('شما این ایمیل را دریافت کردید زیرا ما از اکانت شما درخواست بازیابی رمزعبور دریافت کرده‌ایم.')
+                    ->action('Reset Password', url('password/reset/token', ['token' => $this->token]))
+                    ->line('اگر شما درخواست بازیابی رمزعبور ارسال نکرده‌اید، اقدامی نکنید.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
