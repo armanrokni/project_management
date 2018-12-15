9538972a7901646e708f32b4c984b9acdd050bad
saeed <saeedranjbar.sari@gmail.com>
2018-12-15 18:15:42 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -51,16 +51,3 @@ Route::group(['namespace' => 'Auth'],function(){
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
-#technology controller routes
-Route::get('/technology' , 'TechnologyController@show');
-Route::post('/technology' , 'TechnologyController@add');
-Route::get('/technology/delete/{id}' , 'TechnologyController@delete');
-Route::get('/technology/edit/{id}' , 'TechnologyController@edit');
-Route::post('/technology/update/{id}' , 'TechnologyController@update');
-
-#expertise controller routes
-Route::get('/expertise' , 'ExpertiseController@show');
-Route::post('/expertise' , 'ExpertiseController@add');
-Route::get('/expertise/edit/{id}' , 'ExpertiseController@edit')->where('id', '[0-9]+');
-Route::post('/expertise/update/{id}' , 'ExpertiseController@update')->where('id', '[0-9]+');
-Route::get('/expertise/delete/{id}' , 'ExpertiseController@delete')->where('id', '[0-9]+');
