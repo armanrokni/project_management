4d28c38c16319c901e534eb701d8f7119bdbfc5c
Elham Sadeghian <elhamsadeghian72@gmail.com>
2018-12-16 13:45:07 +0330

    changing reset.blade

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -29,11 +29,11 @@
     </head>
     <body>
 
-<div class="container">
+<div class="container" style="text-align:center; margin:autofocus">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('بازیابی رمزعبور') }}</div>
+                <div class="card-header">  <h1 style="text-align:center"> {{ __('بازیابی رمزعبور') }} </h1> </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.reset') }}">
@@ -41,10 +41,10 @@
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="form-group row">
+                        <div class="form-group row form-data form-input">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('پست الکترونیک') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -55,10 +55,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row form-data form-input">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('رمزعبور') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -69,15 +69,15 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row form-data form-input">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('تایید رمزعبور') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-0 form-data">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('بازیابی رمزعبور') }}
