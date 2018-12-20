59059b8172eb4aeac802556407a81333ed386f56
saeed-rb <saeedranjbar.sari@gmail.com>
2018-12-20 13:26:19 +0330

    adding some change

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'checkLogin' => \App\Http\Middleware\checkLogin::class,
+        'CheckLogin' => \App\Http\Middleware\CheckLogin::class,
         'checkRole' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
