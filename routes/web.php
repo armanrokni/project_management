f38a3baf02eabacec16cb1637b610b1c8bf04cc8
saeed-rb <saeedranjbar.sari@gmail.com>
2018-12-23 10:28:05 +0330

    added some routes

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use App\Http\Middleware\CheckLogin;
 use Illuminate\Http\Request;
 use App\Libraries\Sms;
 
-Route::group(['middleware' => ['CheckLogin', 'checkRole']], function(){
+Route::group(['middleware' => ['checkLogin', 'checkRole']], function(){
     Route::get('/', 'DashboardController@index');
 });
 
-Route::group(['prefix' => 'admin', 'middleware' => ['CheckLogin', 'checkRole']], function(){
+Route::group(['prefix' => 'admin', 'middleware' => ['checkLogin', 'checkRole']], function(){
     Route::group(['prefix' => 'users'], function(){
         Route::get('insert','UserController@insertView' );
         Route::post('insert', 'UserController@insert');
@@ -34,6 +34,23 @@ Route::group(['middleware' => 'checkLogin'], function(){
     Route::get('profile', 'ProfileController@index');
     Route::post('/profile/update', 'ProfileController@update');
 });
+
+// Auth::routes();
+
+Route::group(['namespace' => 'Auth'],function(){
+    // Authentication Routes...
+    Route::get('login', 'LoginController@showLoginForm')->name('login');
+    Route::post('login', 'LoginController@login');
+    Route::get('logout', 'LoginController@logout')->name('logout');
+
+    // Password Reset Routes...
+    Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/token/{token}', 'ResetPasswordController@showResetForm')->name('password.reset.token');
+    Route::post('password/reset', 'ResetPasswordController@reset')->name('password.update');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 #technology controller routes
 Route::get('/technology' , 'TechnologyController@show');
